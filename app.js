a0af3ac983f948c8f427766339269f5976b4d491
Kasemaa <92432832+Kasemaa@users.noreply.github.com>
2021-11-30 16:23:13 +0200

    add delete single function

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
 const form  = document.querySelector('form')
 form.addEventListener('submit', addTask)
 
+// tasklist
+const taskList = document.querySelector('ul')
+taskList.addEventListener('click', delTask)
+
+// delTask
+function delTask(event){
+    if(event.target.textContent === 'X'){
+        if(confirm('Oled kindel?')) {
+            event.target.parentElement.remove()
+        }
+    }
+}
 
 // add task function
 function addTask(event){
@@ -17,6 +29,16 @@ const taskList = document.querySelector('ul')
     //add text to element
     const text = document.createTextNode(task)
     li.appendChild(text)
+    // create link
+    const link = document.createElement('a')
+    // add css style
+    link.className = 'secondary-content'
+    // add text to link
+    link.appendChild(document.createTextNode('X'))
+    // add href attribute
+    link.setAttribute('href', '#')
+    // add link to Li
+    li.appendChild(link)
     // add li to tasklist
     taskList.appendChild(li)
     // clear from input value
